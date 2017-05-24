27c5cbda1fba83367fed4c145c80bbab260aca15
Sam Vohr <svohr@soe.ucsc.edu>
2017-05-24 14:40:39 -0700

    Added setup.py to keep track of dependancies.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tjbd',
+    version='0.0.0',
+    description='',
+    url='http://github.com/svohr/tjbd',
+    author='Samuel H. Vohr',
+    author_email='svohr@soe.ucsc.edu',
+    license='MIT',
+    install_requires=['numpy', 'scipy', 'pysam', 'intervaltree']
+    )
