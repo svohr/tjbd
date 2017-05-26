7cd5c85b520191eaaa75e394269a48914af2e20e
svohr <shvohr@gmail.com>
2017-05-26 16:16:40 -0700

    Added file and stub for hmm decoding.

---
+++ ibd_hmm.py
@@ -0,0 +1,16 @@
+"""
+ibd_hmm.py
+
+This file contains functions for applying the forward-backward algorithm
+to obtain posterior probabilities for the hidden states (IBD and No IBD)
+for each position in a chromosome.
+
+"""
+
+import recmap
+import freqs
+
+def forward_backward():
+    """
+    """
+    pass
