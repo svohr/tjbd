9e4099f1cfe0ec8f5c6b51c649eb539b52310d6a
Sam Vohr <svohr@soe.ucsc.edu>
2017-05-30 15:58:38 -0700

    HMM input simulation from VCF file now works.

--- fake_test.py
+++ fake_test.py
@@ -12,6 +12,7 @@ Mon May 29 15:37:59 PDT 2017
 import sys
 import random
 import argparse
+import itertools
 
 import numpy
 import pysam
@@ -34,7 +35,7 @@ def match_found(hi_geno, lo_obs):
     return True
 
 
-def simulate_hmm_input(vcf_in, ibd_segs, lo_indv, hi_indv, coverage)
+def simulate_hmm_input(vcf_in, ibd_segs, lo_indv, hi_indv, coverage):
     """
     Simulates low-coverage sequencing from a recent historical ancestor (low_i)
     of a present-day individul (hi_indv) by sampling SNP entries from a VCF
@@ -61,8 +62,11 @@ def simulate_hmm_input(vcf_in, ibd_segs, lo_indv, hi_indv, coverage)
     for rec in vcf_in:
         if numpy.random.poisson(coverage) != 1:
             continue # position was not observed or observed too many times.
-        hi_geno = rec.samples[hi_indv]['GT']
-        lo_geno = random.choice(rec.samples[lo_indv]['GT'])
+        _ = rec.alts # this avoids a segfault ¯\_(ツ)_/¯
+        hi_samp = rec.samples[hi_indv]
+        lo_samp = rec.samples[lo_indv]
+        hi_geno = hi_samp['GT']
+        lo_geno = random.choice(lo_samp['GT'])
         if ibd_segs.overlaps(rec.pos):
             if random.choice(True, False):
                 # observing from IBD segment.
@@ -81,9 +85,9 @@ def main():
 
     ibd_segs = pick_ibd_segments()
     with pysam.VariantFile(args.vcf_fn, 'r') as vcf_in:
-        pos, obs = simulate_hmm_input(vcf_in, ibd_segs, 1, 2, 0.01)
+        pos, obs = simulate_hmm_input(vcf_in, ibd_segs, "HG00097", "HG00099", 0.01)
         for p, o in itertools.izip(pos, obs):
-            print p, o, ibd_segs.overlaps(p)
+            print p, "match" if o else "no-match", "IBD" if ibd_segs.overlaps(p) else "no-IBD"
     return 0
 
 
