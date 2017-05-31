4b6a5d0ba58aa6b2f0c38cc91a0d917e64cb2430
Sam Vohr <svohr@soe.ucsc.edu>
2017-05-31 14:51:43 -0700

    fake_test.py now runs a simulated chromosome 21 through the HMM.

--- fake_test.py
+++ fake_test.py
@@ -18,10 +18,14 @@ import numpy
 import pysam
 import intervaltree
 
+import freqs
+import recmap
+import ibd_hmm
+
 
 def pick_ibd_segments():
     ibd = intervaltree.IntervalTree()
-    ibd[10000000:30000000] = 1
+    ibd[19000000:25000000] = 1
     return ibd
 
 
@@ -57,37 +61,58 @@ def simulate_hmm_input(vcf_in, ibd_segs, lo_indv, hi_indv, coverage):
                base matched one of the present-day bases.
     """
     obs_pos = list()
-    obs_state = list()
+    obs_hi_geno = list()
+    obs_lo_geno = list()
 
     for rec in vcf_in:
         if numpy.random.poisson(coverage) != 1:
             continue # position was not observed or observed too many times.
-        _ = rec.alts # this avoids a segfault ¯\_(ツ)_/¯
+        _ = rec.alts # this avoids a segfault
         hi_samp = rec.samples[hi_indv]
         lo_samp = rec.samples[lo_indv]
         hi_geno = hi_samp['GT']
         lo_geno = random.choice(lo_samp['GT'])
         if ibd_segs.overlaps(rec.pos):
-            if random.choice(True, False):
+            if random.choice([True, False]):
                 # observing from IBD segment.
                 lo_geno = hi_geno[0]
             # else: observing from other chromosome.
         obs_pos.append(rec.pos)
-        obs_state.append(match_found(hi_geno, lo_geno))
-    return numpy.array(obs_pos), numpy.array(obs_state)
+        obs_hi_geno.append(hi_geno)
+        obs_lo_geno.append(lo_geno)
+    return numpy.array(obs_pos), obs_hi_geno, obs_lo_geno
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("vcf_fn", metavar="vars.vcf[.gz]", type=str,
                         help="VCF file containing genotypes for individuals")
+    parser.add_argument("frq_fn", metavar="frqs.count", type=str,
+                        help="Allele counts from VCFTools")
+    parser.add_argument("rec_fn", metavar="recmap.tab", type=str,
+                        help="Genetic distances in tab file.")
     args = parser.parse_args()
 
     ibd_segs = pick_ibd_segments()
+
+    frqs = freqs.AlleleFreqs()
+    with open(args.frq_fn, 'r') as frq_in:
+        frqs.read_vcf_counts(frq_in)
+
+    rmap = recmap.RecMap()
+    with open(args.rec_fn, 'r') as rec_in:
+        rmap.read_tab(rec_in)
+
     with pysam.VariantFile(args.vcf_fn, 'r') as vcf_in:
-        pos, obs = simulate_hmm_input(vcf_in, ibd_segs, "HG00097", "HG00099", 0.01)
-        for p, o in itertools.izip(pos, obs):
-            print p, "match" if o else "no-match", "IBD" if ibd_segs.overlaps(p) else "no-IBD"
+        pos, obs_hi, obs_lo = simulate_hmm_input(vcf_in, ibd_segs, "HG00097", "HG00099", 0.05)
+        obs = [match_found(h, l) for h, l in itertools.izip(obs_hi, obs_lo)]
+        lo_freq = numpy.array([frqs.frequency("21", p, b) for p, b in itertools.izip(pos, obs_lo)])
+
+        ibd_trs, noibd_trs = ibd_hmm.state_trans(rmap, 5, "21", pos)
+        probs = ibd_hmm.forward_backward(5, obs, lo_freq, ibd_trs, noibd_trs)
+        for p, o, f, i, n, prob in itertools.izip(pos, obs, lo_freq, ibd_trs, noibd_trs, probs):
+            print p, "match" if o else "no-match", "IBD" if ibd_segs.overlaps(p) else "no-IBD", f, i, n, prob
+
     return 0
 
 
