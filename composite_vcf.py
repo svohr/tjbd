ba7ac11abf8334aeab3749dd6d3a6263dd4b83e8
Sam Vohr <svohr@soe.ucsc.edu>
2017-06-12 11:14:41 -0700

    Added stub for script for generating composite individuals

---
+++ composite_vcf.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python2
+"""
+This script takes in a VCF file containing the genotypes of N individuals
+individuals and generates a VCF file containing N/2 composite inviduals using
+the method described by Browning and Browning 2010.
+"""
+
+import sys
+import argparse
+
+
+def main():
+    """
+    do thing.
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
